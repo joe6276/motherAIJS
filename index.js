555400c97c3e3491885b94437f21915836c44bc5
Jonathan Ndambuki <joendambuki16@gmail.com>
2025-05-08 13:40:24 +0300

    initial Commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express= require("express")
 const {json} = require("express")
 const cors = require('cors')
 
+const _= require("./Controllers/telegramBot")
 const { router } = require("./Routes/AiRoutes")
 const { userRouter } = require("./Routes/userRoutes")
 const { companyRouter } = require("./Routes/companyRoutes")
